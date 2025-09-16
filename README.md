b0642bbf05e40502c4ac290153b6c928bc0f6a91
Michael Arndt <arndt@arndt-mac-0.localdomain>
2025-09-16 09:59:57 -0400

    Added -check option with target utilization input.

--- README.md
+++ README.md
@@ -15,18 +15,34 @@ Setup Instructions:
 
 3. Run the script with CLI options:
   $ volume_compression_resize.py -cluster <CLUSTER> -aggr <aggr> -user <USER>
+                                 [-check] [-target <percentage>]
+                                 [-debug] [-xml]
+
+Examples:
+1. Print out volume resize commands for all volumes to grow active filesystems
+   the same amount as the current compression savings for each volume. This
+   approach may be more useful before performing a volume move.
+     volume_compression_resize.py -cluster cluster1 -aggr aggr1 -user admin
+2. Print out volume resize commands for volumes that would be over 80% full
+   without compression savings, resizing them to reach 80% full if all current
+   compression savings were lost at the volume level.  This approach may be
+   more useful after a volume move was performed without a resize.
+     volume_compression_resize.py -cluster cluster1 -aggr aggr1 -user admin \
+     -check -target 80
 
 FlexGroup notes:
 1. FlexGroups are likely to have constituent volumes moved at various times.
-2. Recommendations are made to keep the FlexGroup no more than 90% full.
+2. It is recommended to keep FlexGroups no more than 90% full.
 3. ONTAP will automatically try to keep all FlexGroup constituents at a
    similar level of free space.
+4. Extra detail is printed for FlexGroup volumes to aid in sizing decisions.
 ```
 
 Using volume_compression_resize.py
 ```
 usage: volume_compression_resize.py [-h] -cluster CLUSTER -aggr AGGR -user
-                                    USER [-debug] [-xml]
+                                    USER [-check] [-target TARGET] [-debug]
+                                    [-xml]
 
 Retrieve NetApp volume data and calculate recommended size increase.
 
@@ -35,6 +51,9 @@ optional arguments:
   -cluster CLUSTER  Cluster hostname or IP
   -aggr AGGR        Aggregate name to filter volumes
   -user USER        Username for authentication
+  -check            Only check for volumes that would be over 'target' full
+                    percentage
+  -target TARGET    Target volume full percentage (default: 90)
   -debug            Enable debug
   -xml              Enable xml debug
 ```
--- volume_compression_resize.py
+++ volume_compression_resize.py
@@ -15,12 +15,27 @@
 #
 # 3. Run the script with CLI options:
 #   $ volume_compression_resize.py -cluster <CLUSTER> -aggr <aggr> -user <USER>
+#                                  [-target <percentage>] [-check]
+#                                  [-debug] [-xml]
+#
+# Examples:
+# 1. Print out volume resize commands for all volumes to grow active filesystems
+#    the same amount as the current compression savings for each volume. This
+#    approach may be more useful before performing a volume move.
+#      volume_compression_resize.py -cluster cluster1 -aggr aggr1 -user admin
+# 2. Print out volume resize commands for volumes that would be over 80% full
+#    without compression savings, resizing them to reach 80% full if all current
+#    compression savings were lost at the volume level.  This approach may be
+#    more useful after a volume move was performed without a resize.
+#      volume_compression_resize.py -cluster cluster1 -aggr aggr1 -user admin \
+#      -check -target 80
 #
 # FlexGroup notes:
 # 1. FlexGroups are likely to have constituent volumes moved at various times.
-# 2. Recommendations are made to keep the FlexGroup no more than 90% full.
+# 2. It is recommended to keep FlexGroups no more than 90% full.
 # 3. ONTAP will automatically try to keep all FlexGroup constituents at a
 #    similar level of free space.
+# 4. Extra detail is printed for FlexGroup volumes to aid in sizing decisions.
 #
 ################################################################################
 
@@ -34,11 +49,13 @@ from netapp_ontap.resources import Volume
 # Get CLI arguments.
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Retrieve NetApp volume data and calculate recommended size increase."
+        description="Retrieve NetApp volume data and calculate recommended size increases."
     )
     parser.add_argument("-cluster", required=True, help="Cluster hostname or IP")
     parser.add_argument("-aggr", required=True, help="Aggregate name to filter volumes")
     parser.add_argument("-user", required=True, help="Username for authentication")
+    parser.add_argument("-check", action='store_true', required=False, help="Only check for volumes that would be over 'target' full percentage")
+    parser.add_argument("-target", required=False, type=int, default='90', help="Target volume full percentage (default: 90)")
     parser.add_argument("-debug", action='store_true', required=False, help="Enable debug")
     parser.add_argument("-xml", action='store_true', required=False, help="Enable xml debug")
     return parser.parse_args()
@@ -74,38 +91,43 @@ def main():
         style = volume.style
         compression_saved = volume.efficiency.space_savings.compression
         snapshot_percent = volume.space.snapshot.reserve_percent
-        available = volume.space.available
         used = volume.space.used
+        available = volume.space.available
 
-        # Calculate the required increase.
+        # Perform the required calculations.
         recommended_increase = ceil(compression_saved / ((100-snapshot_percent)/100))
+        afs_size = used + available
+        used_wo_compression = used + compression_saved
+        used_percent_wo_compression = int(100 * ((used_wo_compression) / afs_size))
+        available_gb = int(available / (1024*1024*1024))
+        compression_saved_gb = ceil(compression_saved / (1024*1024*1024))
 
         # Debugging if required.
         if args.debug:
-            print(f"DEBUG: {svm}:{vol_name} compression_saved:{compression_saved} snap_reserve:{snapshot_percent}")
-            print(f"DEBUG: {svm}:{vol_name} Recommended Size Increase: {recommended_increase} bytes")
+            print(f"DEBUG: {svm}:{vol_name} compression_saved:{compression_saved} snap_reserve:{snapshot_percent}.")
+            print(f"DEBUG: {svm}:{vol_name} style:{style} used:{used} available:{available}.")
+            print(f"DEBUG: {svm}:{vol_name} Recommended Size Increase: {recommended_increase} bytes.")
         if args.xml:
             print("START XML")
             pprint.pprint(volume.to_dict())
             print("END XML")
 
-        # Print out FlexVol volume resize recommended command.
-        if style == "flexvol" and compression_saved:
-            print(f"volume size -vserver {svm} -volume {vol_name} -new-size +{recommended_increase}")
-
-        # Perform calculations and print FlexGroup recommendations.
-        if style == "flexgroup" and compression_saved:
-            available_gb = int(available / (1024*1024*1024))
-            compression_saved_gb = ceil(compression_saved / (1024*1024*1024))
-            afs_size = used + available
-            used_wo_compression = used + compression_saved
-            used_percent_wo_compression = int(100 * ((used_wo_compression) / afs_size))
-            print(f"FlexGroup '{vol_name}' has {available_gb}GB available and {compression_saved_gb}GB saved by compression.")
-            print(f"FlexGroup '{vol_name}' capacity utilization without compression savings would be {used_percent_wo_compression}%")
-            if used_percent_wo_compression > 90:
-                target_90_capacity = ceil((used_wo_compression / .9) - afs_size)
-                target_90_capacity_gb = ceil(target_90_capacity / (1024*1024*1024))
-                print(f"FlexGroup '{vol_name}' needs an extra {target_90_capacity_gb}GB to stay at 90% capacity without compression savings")
+        # Print out the volume resize recommended command as required.
+        if compression_saved:
+            # Print out extra information for FlexGroup volumes.
+            if style == "flexgroup":
+                print(f"INFO: FlexGroup '{vol_name}' has {available_gb}GB available and {compression_saved_gb}GB saved by compression.")
+                print(f"INFO: FlexGroup '{vol_name}' capacity utilization without compression savings would be {used_percent_wo_compression}%.")
+            if args.check and used_percent_wo_compression > args.target:
+                target_capacity = ceil((used_wo_compression / (args.target/100)) - afs_size)
+                target_capacity_gb = ceil(target_capacity / (1024*1024*1024))
+                print(f"INFO: '{vol_name}' needs an extra {target_capacity_gb}GB to stay at {args.target}% capacity without compression savings.")
+                print(f"volume size -vserver {svm} -volume {vol_name} -new-size +{target_capacity_gb}g")
+            if not args.check:
+                if style == "flexgroup":
+                    print(f"INFO: Potential FlexGroup resize command: volume size -vserver {svm} -volume {vol_name} -new-size +{recommended_increase}")
+                else:
+                    print(f"volume size -vserver {svm} -volume {vol_name} -new-size +{recommended_increase}")
 
 if __name__ == "__main__":
     main()
